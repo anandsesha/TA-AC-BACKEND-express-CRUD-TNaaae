ca294162b69b9e32ce4c005a1acec0a3ebcc1277
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-09-09 20:57:13 -0400

    `Update` routers

---
+++ block-BNaade/bookstore/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  uname: { type: String, required: true },
+  uage: Number,
+  uemail: String,
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
